792412b2436ce1e93bb5a99c505c183a13743142
Roland Foucher <roland.foucher.pro@gmail.com>
2022-01-17 11:40:26 +0100

    debug router

--- src/app.js
+++ src/app.js
@@ -9,17 +9,19 @@ const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
 
+
 app.use(express.json());
+
  
-app.use('/shoppingList', (req, res, next) => {
+router.get('/shoppingList', (req, res, next) => {
     res.status(200).json(itemToSale);
     next();
 })
-app.use('/news', (req, res, next) => {
+router.get('/news', (req, res, next) => {
     res.status(200).json(news);
     next();
 })
-app.use('/photos', (req, res, next) => {
+router.get('/photos', (req, res, next) => {
     res.status(200).json(photos);
     next();
 })
@@ -28,9 +30,8 @@ router.get('/test',(req,res, next) => {
       hello: "test!"
     });
     next();
-
 })
-app.post('/contact', (req, res, next) => {
+router.post('/contact', (req, res, next) => {
     console.log(req.body);
     res.status(201).json(req.body);
     next();
